3a3cc8caaa357590c4cdb6f785beb142a058ba9f
WickyNilliams <WickyNilliams@MBA>
2013-08-24 15:52:18 +0300

    move all vendor scripts to own folder

---
+++ assets/scripts/vendor/jQuery.headroom.js
@@ -0,0 +1,41 @@
+/*!
+ * headroom.js v0.3.5 - Give your page some headroom. Hide your header until you need it
+ * Copyright (c) 2013 Nick Williams - http://wicky.nillia.ms/
+ * License: MIT
+ */
+
+;(function($) {
+
+	////////////
+	// Plugin //
+	////////////
+
+	$.fn.headroom = function(option) {
+		return this.each(function() {
+			var $this   = $(this),
+				data    = $this.data('headroom'),
+			    options = typeof option === 'object' && option;
+
+		    options = $.extend(true, {}, Headroom.options, options);
+
+			if (!data) {
+				data = new Headroom(this, options);
+				data.init();
+				$this.data('headroom', data);
+		    }
+			if (typeof option === 'string') {
+				data[option]();
+			}
+		});
+	};
+
+	//////////////
+	// Data API //
+	//////////////
+
+	$('[data-headroom]').each(function() {
+		var $this = $(this);
+		$this.headroom($this.data());
+	});
+
+}(window.Zepto || window.jQuery));
