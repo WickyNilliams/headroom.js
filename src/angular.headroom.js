ffcfe46c026cd75a5c2b74161fe0a95e91c61ed5
☃ pitr <pitr.vern@gmail.com>
2013-12-09 13:00:47 -0500

    add angular.headroom.js

---
+++ src/angular.headroom.js
@@ -0,0 +1,28 @@
+(function(angular) {
+
+  ///////////////
+  // Directive //
+  ///////////////
+
+  angular.module('headroom', []).directive('headroom', function() {
+    return {
+      restrict: 'EA',
+      scope: {
+        tolerance: '=',
+        offset: '=',
+        classes: '='
+      },
+      link: function(scope, element) {
+        var options = {};
+        angular.forEach(Headroom.options, function(value, key) {
+          options[key] = scope[key] || Headroom.options[key];
+        });
+        var headroom = new Headroom(element[0], options).init();
+        scope.$on('destroy', function() {
+          headroom.destroy();
+        });
+      }
+    };
+  });
+
+}(window.angular));
