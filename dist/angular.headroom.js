3029bb7bbf7e9def141a4e685858f96d8a2cee31
WickyNilliams <WickyNilliams@MBA>
2014-01-09 20:31:36 +0000

    v0.4.0

--- dist/angular.headroom.js
+++ dist/angular.headroom.js
@@ -1,10 +1,14 @@
 /*!
- * headroom.js v0.3.11 - Give your page some headroom. Hide your header until you need it
- * Copyright (c) 2013 Nick Williams - http://wicky.nillia.ms/headroom.js
+ * headroom.js v0.4.0 - Give your page some headroom. Hide your header until you need it
+ * Copyright (c) 2014 Nick Williams - http://wicky.nillia.ms/headroom.js
  * License: MIT
  */
 
 (function(angular) {
+
+  if(!angular) {
+    return;
+  }
 
   ///////////////
   // Directive //
