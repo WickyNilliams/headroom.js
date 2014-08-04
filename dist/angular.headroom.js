3f9cda575a62d4e3c4693138b5a90526f58257a9
WickyNilliams <WickyNilliams@MBA>
2014-08-04 20:40:20 +0100

    bump package version

--- dist/angular.headroom.js
+++ dist/angular.headroom.js
@@ -1,5 +1,5 @@
 /*!
- * headroom.js v0.6.0 - Give your page some headroom. Hide your header until you need it
+ * headroom.js v0.7.0 - Give your page some headroom. Hide your header until you need it
  * Copyright (c) 2014 Nick Williams - http://wicky.nillia.ms/headroom.js
  * License: MIT
  */
@@ -20,13 +20,17 @@
       scope: {
         tolerance: '=',
         offset: '=',
-        classes: '='
+        classes: '=',
+        scroller: '@'
       },
       link: function(scope, element) {
         var options = {};
         angular.forEach(Headroom.options, function(value, key) {
           options[key] = scope[key] || Headroom.options[key];
         });
+        if (options.scroller) {
+          options.scroller = angular.element(options.scroller)[0];
+        }
         var headroom = new Headroom(element[0], options);
         headroom.init();
         scope.$on('destroy', function() {
--- dist/jQuery.headroom.min.js
+++ dist/jQuery.headroom.min.js
@@ -1,5 +1,5 @@
 /*!
- * headroom.js v0.6.0 - Give your page some headroom. Hide your header until you need it
+ * headroom.js v0.7.0 - Give your page some headroom. Hide your header until you need it
  * Copyright (c) 2014 Nick Williams - http://wicky.nillia.ms/headroom.js
  * License: MIT
  */
