3f3c2085aa4146af837dd887a8195e22b9620ebc
Mofasa <nigbit@qq.com>
2014-10-24 10:32:20 +0800

    add update callback #121

--- src/Headroom.js
+++ src/Headroom.js
@@ -302,6 +302,8 @@ Headroom.prototype = {
     else if(this.shouldPin(currentScrollY, toleranceExceeded)) {
       this.pin();
     }
+    
+    this.onScroll && this.onScroll.call(this, this.lastKnownScrollY, currentScrolly);
 
     this.lastKnownScrollY = currentScrollY;
   }
