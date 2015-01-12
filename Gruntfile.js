7b115bc34acba1a8adc849fd3724391360c94bc1
WickyNilliams <WickyNilliams@MBA>
2015-01-12 19:27:11 +0000

    UMD support. Fixes #97

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,14 +63,6 @@ module.exports = function(grunt) {
         files : {
           src : ['spec/*.js']
         }
-      },
-      postbuild : {
-        options : {
-          jshintrc : '.jshintrc'
-        },
-        files :{
-          src : ['<%= meta.output %>']
-        }
       }
     },
 
@@ -118,6 +110,6 @@ module.exports = function(grunt) {
 
   grunt.registerTask('prehint', ['jshint:prebuild', 'jshint:tests']);
   grunt.registerTask('ci', ['prehint', 'karma:continuous']);
-  grunt.registerTask('dist', ['ci', 'rig', 'jshint:postbuild', 'uglify']);
+  grunt.registerTask('dist', ['ci', 'rig', 'uglify']);
   grunt.registerTask('default', ['karma:unit:start', 'watch']);
 };
--- src/include/wrapper.js
+++ src/include/wrapper.js
@@ -1,11 +1,24 @@
-(function(window, document) {
+(function(root, factory) {
+  'use strict';
 
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], factory);
+  }
+  else if (typeof exports === 'object') {
+    // COMMONJS
+    module.exports = factory();
+  }
+  else {
+    // BROWSER
+    root.Headroom = factory();
+  }
+}(this, function() {
   'use strict';
 
   //= ../features.js
   //= ../Debouncer.js
   //= ../Headroom.js
 
-  window.Headroom = Headroom;
-
-}(window, document));
+  return Headroom;
+}));
