2bbf73eb0767f20d8e1afca05e54d06f5dc19025
WickyNilliams <WickyNilliams@MBA>
2019-08-22 12:46:53 +0100

    integrate rollup-plugin-license

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import license from "rollup-plugin-license";
+
 export default {
   input: "src/Headroom.js",
   output: {
@@ -5,5 +7,15 @@ export default {
     format: "umd",
     name: "Headroom",
     compact: true
-  }
+  },
+  plugins: [
+    license({
+      banner: {
+        commentStyle: "ignored",
+        content: `<%= pkg.name %> v<%= pkg.version %> - <%= pkg.description %>
+                  Copyright (c) <%= moment().format('YYYY') %> <%= pkg.author %> - <%= pkg.homepage %>
+                  License: <%= pkg.license %>`
+      }
+    })
+  ]
 };
