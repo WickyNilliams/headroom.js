6c925b00792287b79366c74b6f6eef626f705ea4
WickyNilliams <WickyNilliams@MBA>
2019-08-22 13:09:43 +0100

    separate minified and unminified outputs

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,41 @@
 import license from "rollup-plugin-license";
 import { uglify } from "rollup-plugin-uglify";
 
-export default {
-  input: "src/Headroom.js",
+const input = "src/Headroom.js";
+
+const output = {
+  format: "umd",
+  name: "Headroom"
+};
+
+const plugins = [
+  license({
+    banner: {
+      commentStyle: "ignored",
+      content: `<%= pkg.name %> v<%= pkg.version %> - <%= pkg.description %>
+                Copyright (c) <%= moment().format('YYYY') %> <%= pkg.author %> - <%= pkg.homepage %>
+                License: <%= pkg.license %>`
+    }
+  })
+];
+
+const unminified = {
+  input,
   output: {
-    file: "dist/headroom.js",
-    format: "umd",
-    name: "Headroom",
+    ...output,
+    file: "dist/headroom.js"
+  },
+  plugins
+};
+
+const minified = {
+  input,
+  output: {
+    ...output,
+    file: "dist/headroom.min.js",
     compact: true
   },
-  plugins: [
-    uglify(),
-    license({
-      banner: {
-        commentStyle: "ignored",
-        content: `<%= pkg.name %> v<%= pkg.version %> - <%= pkg.description %>
-                  Copyright (c) <%= moment().format('YYYY') %> <%= pkg.author %> - <%= pkg.homepage %>
-                  License: <%= pkg.license %>`
-      }
-    })
-  ]
+  plugins: [uglify(), ...plugins]
 };
+
+export default [unminified, minified];
